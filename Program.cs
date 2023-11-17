66fefced27a71e6c09f7d74426e5471be17f5b85
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-17 20:22:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Threads
+{
+    public class SharedRecources
+    {
+        public static int SharedResouce { get; set; }
+
+        public static readonly object lockObject = new();
+    }
+
+    class NumberUpCounter
+    {
+        public int Count { get; set; }
+
+        public void CountUp(Action<long> callBack)
+        {
+            long sum = 0;
+
+            try
+            {
+                Console.WriteLine("Count up started");
+                Thread.Sleep(1000);
+
+                for (int i = 1; i <= Count; i++)
+                {
+                    sum += i;
+
+                    lock (SharedRecources.lockObject)
+                    {
+                        Console.WriteLine($"Shared resource is Count-Up: {SharedRecources.SharedResouce}, ");
+                        SharedRecources.SharedResouce++;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"i = {i}, ");
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                callBack(sum);
+            }
+        }
+    }
+
+    class NumberDownCounter
+    {
+        public int Count { get; set; }
+
+        public void CounDown(Action<long> callBack)
+        {
+            long sum = 0;
+
+            try
+            {
+                Console.WriteLine("Count down started");
+                Thread.Sleep(1000);
+
+                for (int j = Count; j >= 1; j--)
+                {
+                    lock (SharedRecources.lockObject) 
+                    {
+                        Console.WriteLine($"Shared resource is Count-Down: {SharedRecources.SharedResouce}, ");
+                        SharedRecources.SharedResouce--;
+                    }
+                    
+                    Monitor.Exit(SharedRecources.lockObject);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"j = {j}, ");
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                callBack(sum);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberDownCounter counterDown = new() { Count = 100 };
+            NumberUpCounter counterUp = new() { Count = 100 };
+
+            void callBack(long sum)
+            {
+                Console.WriteLine($"Return value from is sum {sum}");
+            }
+
+            ThreadStart thread = new(() => { counterDown.CounDown(callBack); });
+            ThreadStart thread1 = new(() => { counterUp.CountUp(callBack); });
+            Thread th1 = new(thread);
+            Thread th2 = new(thread1);
+            th2.Start();
+            th1.Start();
+        }
+    }
+}
